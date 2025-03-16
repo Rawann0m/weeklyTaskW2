a13312c6a312449f99978b83258c1a68b85b3c1b
Rawann0m <raawannm@gmail.com>
2025-03-16 09:49:57 +0300

    Initial Commit

---
+++ weeklyTaskW2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weeklyTaskW2
+//
+//  Created by Rawan on 16/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weeklyTaskW2/weeklyTaskW2App.swift
@@ -0,0 +1,17 @@
+//
+//  weeklyTaskW2App.swift
+//  weeklyTaskW2
+//
+//  Created by Rawan on 16/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct weeklyTaskW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
